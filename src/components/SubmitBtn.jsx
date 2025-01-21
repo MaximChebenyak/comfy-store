9206b1ca9983e69cbd4c4b1dd44a586864f1cf67
maxim <maximchebenyak@gmail.com>
2025-01-21 22:15:23 +0200

    register page

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,8 +1,22 @@
+import { useNavigation } from 'react-router-dom';
+
 // eslint-disable-next-line react/prop-types
 const SubmitBtn = ({ text }) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
   return (
-    <button type="submit" className="btn btn-primary capitalize">
-      {text}
+    <button
+      type="submit"
+      className="btn btn-primary btn-block capitalize "
+      disabled={isSubmitting}
+    >
+      {isSubmitting ? (
+        <>
+          <span className="loading loading-spinner">sending...</span>
+        </>
+      ) : (
+        text || 'submit'
+      )}
     </button>
   );
 };
