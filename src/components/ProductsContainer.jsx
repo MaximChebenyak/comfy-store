e209a0551609374a3376257a0b58540f987e489d
maxim <maximchebenyak@gmail.com>
2025-01-25 17:29:00 +0200

    products list

---
+++ src/components/ProductsContainer.jsx
@@ -0,0 +1,12 @@
+import ProductsGrid from './ProductsGrid';
+import ProductList from './ProductsList';
+
+const ProductsContainer = () => {
+  return (
+    <>
+      <ProductList />
+      <ProductsGrid />
+    </>
+  );
+};
+export default ProductsContainer;
